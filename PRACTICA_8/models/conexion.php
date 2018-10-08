3f69a95f115fe8548a8f4b9e593669ea326484b3
Rodrigo <1530219@upv.edu.mx>
2018-10-08 12:21:49 -0500

    8 commit

--- PRACTICA_8/models/conexion.php
+++ PRACTICA_8/models/conexion.php
@@ -3,7 +3,7 @@
 class Conexion{
 
     public function conectar(){
-        $link = new PDO("mysql:host=localhost;dbname=usuarios", "root", "");
+        $link = new PDO("mysql:host=localhost;dbname=usuarios", "admin", "343c1c78286393abc99e44ae8bfd77d764218a6540750165");
         return $link;
     }
 
