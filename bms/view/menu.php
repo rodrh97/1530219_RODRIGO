599ffcc164777450767efadfb548f19a0e7bf2a6
Rodrigo <1530219@upv.edu.mx>
2018-10-23 15:41:35 -0500

    BMS CON DISEÑO

--- bms/view/menu.php
+++ bms/view/menu.php
@@ -3,11 +3,11 @@
 	{
 ?>
 <center>
-<div class="ui compact menu">
-	<a class="browse item" href="index.php?page=index">Home</a>
-	<a class="browse item" href="index.php?page=register">Register</a>
-	<a class="browse item" href="index.php?page=login">Login</a>
-	<a class="browse item" href="index.php?page=forgot_password">Forgot Password</a>
+<div class="ui blue four item inverted menu">
+	<a class="browse item" href="index.php?page=index"><i class="fas fa-home"></i>Home</a>
+	<a class="browse item" href="index.php?page=register"><i class="fas fa-user-plus"></i>Register</a>
+	<a class="browse item" href="index.php?page=login"><i class="fas fa-sign-in-alt"></i>Login</a>
+	<a class="browse item" href="index.php?page=forgot_password"><i class="fas fa-question"></i>Forgot Password</a>
 </div>
 </center>
 <?php
@@ -16,12 +16,12 @@
 	{
 ?>
 <center>
-<div class="ui compact menu">
-	<a class="browse item" href="index.php?page=home">Home</a>
-	<a class="browse item" href="index.php?page=profile">Profile</a>
-	<a class="browse item" href="index.php?page=book_add">Add Book</a>
-	<a class="browse item" href="index.php?page=book_list">List Book</a>
-	<a class="browse item" href="index.php?page=logout">Logout</a>
+<div class="ui blue five item inverted menu">
+	<a class="browse item" href="index.php?page=home"><i class="fas fa-home"></i>Home</a>
+	<a class="browse item" href="index.php?page=profile"><i class="far fa-user"></i></i>Profile</a>
+	<a class="browse item" href="index.php?page=book_add"><i class="fas fa-plus-circle"></i>Add Book</a>
+	<a class="browse item" href="index.php?page=book_list"><i class="fas fa-list-ul"></i>List Book</a>
+	<a class="browse item" href="index.php?page=logout"><i class="fas fa-sign-out-alt"></i>Logout</a>
 </div>
 </center>
 <?php
